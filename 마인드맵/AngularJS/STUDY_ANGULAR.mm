fb09f6f4a67e133e17b7bd99a6305ca4078ea9d5
neem693 <37389515+neem693@users.noreply.github.com>
2018-10-11 19:15:38 +0900

    Update 마인드맵/AngularJS/STUDY_ANGULAR.mm

---
+++ 마인드맵/AngularJS/STUDY_ANGULAR.mm
@@ -0,0 +1,94 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1539216415402" ID="ID_82650560" MODIFIED="1539217147161" TEXT="AngularJS">
+<hook NAME="accessories/plugins/CreationModificationPlugin_new.properties"/>
+<hook NAME="accessories/plugins/RevisionPlugin.properties"/>
+<node COLOR="#3333ff" CREATED="1539216543516" ID="ID_766901009" MODIFIED="1539217062930" POSITION="right" STYLE="bubble" TEXT="&#xd3fc; &#xae30;&#xbcf8;&#xac1c;&#xb150;">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node CREATED="1539216516243" ID="ID_374983385" LINK="https://angularjs.org/" MODIFIED="1539217062920" TEXT="https://angularjs.org/"/>
+<node CREATED="1539216516214" ID="ID_1675020178" MODIFIED="1539217062920" TEXT="GET"/>
+<node CREATED="1539216516220" ID="ID_929963510" LINK="http://anylogic.co" MODIFIED="1539217062920" TEXT="anylogic.co,kr &gt; Api &gt; GetUserInfo ? ..."/>
+<node CREATED="1539216516229" ID="ID_405657649" MODIFIED="1539217062920" TEXT="POST"/>
+<node CREATED="1539216516231" ID="ID_148734836" LINK="http://anylogic.co" MODIFIED="1539217062920" TEXT="anylogic.co,kr &gt; Api &gt; GetUserInfo"/>
+<node CREATED="1539216516233" ID="ID_864811135" MODIFIED="1539217062920" TEXT="PUT"/>
+<node CREATED="1539216516234" ID="ID_14132166" LINK="http://anylogic.co" MODIFIED="1539217062920" TEXT="anylogic.co,kr &gt; Api &gt; GetUserInfo"/>
+<node CREATED="1539216516235" ID="ID_1330125897" MODIFIED="1539217062920" TEXT="DELETE"/>
+<node CREATED="1539216516241" ID="ID_1785142681" LINK="http://anylogic.co" MODIFIED="1539217062920" TEXT="anylogic.co,kr &gt; Api &gt; GetUserInfo ? ..."/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516244" ID="ID_1869406348" MODIFIED="1539217063170" POSITION="right" STYLE="bubble" TEXT="ng-model">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node CREATED="1539216516246" ID="ID_1451333499" MODIFIED="1539217063167" TEXT="js&#xc640; html&#xac04; &#xacf5;&#xc720;&#xb418;&#xb294; &#xb370;&#xc774;&#xd130; &#xac1d;&#xccb4;"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516247" ID="ID_529283619" MODIFIED="1539217063345" POSITION="right" STYLE="bubble" TEXT="ng-click">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node CREATED="1539216516248" ID="ID_492528775" MODIFIED="1539217063342" TEXT="html&#xc5d0;&#xc11c; &#xd2c0;&#xb9ad;&#xc2dc; &#xc2e4;&#xd589;&#xb420; js &#xd568;&#xc218;"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516249" ID="ID_1294331707" MODIFIED="1539217064145" POSITION="right" STYLE="bubble" TEXT="ng-repeat">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node COLOR="#338800" CREATED="1539216516249" ID="ID_312871740" MODIFIED="1539217064143" TEXT="&lt;li class=&quot;animate-repeat&quot; ng-repeat=&quot;friend in friends&quot;&gt;"/>
+<node CREATED="1539216516255" ID="ID_1697553841" MODIFIED="1539217064143" TEXT="js&#xc5d0;&#xc11c; $scope.friends&#xc758; &#xbc30;&#xc5f4; &#xac2f;&#xc218;&#xb9cc;&#xd07c; &#xbc18;&#xbcf5;&#xbb38;&#xc774; &#xb3cc;&#xba74;&#xc11c; li &#xac1d;&#xccb4;&#xac00; &#xc0dd;&#xc131;&#xb41c;&#xb2e4;"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516257" ID="ID_406766409" MODIFIED="1539217067081" POSITION="right" STYLE="bubble" TEXT="ng-show / ng-hide(&#xc548;&#xc758; &#xac12;&#xc774; true/false&#xba04; &#xbcf4;&#xc774;&#xace0; &#xc548;&#xbcf4;&#xc774;&#xace0;..)">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516260" ID="ID_583945682" MODIFIED="1539217067265" POSITION="right" STYLE="bubble" TEXT="ng-show=&quot;true&quot;">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516261" ID="ID_1210941228" MODIFIED="1539217067439" POSITION="right" STYLE="bubble" TEXT="ng-change">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node CREATED="1539216516261" ID="ID_1992737216" MODIFIED="1539216516261" TEXT="html&#xc5d0;&#xc11c; &#xc548;&#xc758; &#xb370;&#xc774;&#xd130;&#xac00; &#xbc14;&#xb00c;&#xba74; &#xc2e4;&#xd589;"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516262" ID="ID_1036977019" MODIFIED="1539218106717" POSITION="right" STYLE="bubble" TEXT="HTML">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node CREATED="1539216516264" ID="ID_494640042" MODIFIED="1539218106717" TEXT="&lt;input type=&quot;text ng-mode=&quot;inputText&quot; ng-change=&quot;changed()&quot;&gt;"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516265" ID="ID_199505398" MODIFIED="1539218106909" POSITION="right" STYLE="bubble" TEXT="JS">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node CREATED="1539216516266" ID="ID_1241207229" MODIFIED="1539218106909" TEXT="$scope.changed = function(){"/>
+<node CREATED="1539216516268" ID="ID_65414862" MODIFIED="1539218106909" TEXT="alert($scope.inputText)"/>
+<node CREATED="1539216516269" ID="ID_1359830966" MODIFIED="1539218106909" TEXT="}"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516270" ID="ID_154606063" MODIFIED="1539218107117" POSITION="right" STYLE="bubble" TEXT="HTML">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node CREATED="1539216516271" ID="ID_895290963" MODIFIED="1539218107117" TEXT="&lt;input type=&quot;text ng-mode=&quot;inputText&quot; ng-change=&quot;changed(inputText)&quot;&gt;"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516272" ID="ID_1268799451" MODIFIED="1539218107294" POSITION="right" STYLE="bubble" TEXT="JS">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node CREATED="1539216516272" ID="ID_472438879" MODIFIED="1539218107294" TEXT="$scope.changed = function(str){"/>
+<node CREATED="1539216516273" ID="ID_1794436864" MODIFIED="1539218107294" TEXT="alert(str)"/>
+<node CREATED="1539216516273" ID="ID_1667060291" MODIFIED="1539218107294" TEXT="}"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516274" ID="ID_655376177" MODIFIED="1539217086048" POSITION="right" STYLE="bubble" TEXT="ng-checked">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node COLOR="#3333ff" CREATED="1539216516275" ID="ID_1229381528" MODIFIED="1539217096056" STYLE="bubble" TEXT="&#xccb4;&#xd06c;&#xbc15;&#xc2a4;&#xb97c; &#xc0ac;&#xc6a9;&#xd560;&#xb54c; &#xd574;&#xb2f9; &#xccb4;&#xd06c;&#xbc15;&#xc2a4;&#xac00; &#xccb4;&#xd06c;&#xb418;&#xc5b4; &#xc788;&#xb294;&#xc9c0; &#xc5ec;&#xbd80;&#xac00; true/false&#xb85c; &#xbc18;&#xd658;&#xb428;">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516276" ID="ID_1554976217" MODIFIED="1539217095392" STYLE="bubble" TEXT="&lt;input  type=&quot;checkbox&quot; ng-checked=&quot;leader&quot; &gt;">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+</node>
+<node CREATED="1539216516277" ID="ID_1065304320" MODIFIED="1539216516277" TEXT="JS">
+<node CREATED="1539216516278" ID="ID_868244461" MODIFIED="1539216516278" TEXT="&#xccb4;&#xd06c;&#xb418;&#xc5b4; &#xc788;&#xc73c;&#xba74; $scope.leader == true;"/>
+</node>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516279" ID="ID_1219718322" MODIFIED="1539217108859" POSITION="right" STYLE="bubble" TEXT="ng-disabled">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node CREATED="1539216516280" ID="ID_1174482300" MODIFIED="1539217108857" TEXT="&lt;button ng-model=&quot;button&quot; ng-disabled=&quot;checked&quot;&gt;Button&lt;/button&gt;"/>
+<node CREATED="1539216516281" ID="ID_1128991500" MODIFIED="1539216516281" TEXT="ng-disabled&#xac12;&#xc774; true&#xba74; &#xd574;&#xb2f9; &#xcee8;&#xd2b8;&#xb864;&#xc740; &#xc0ac;&#xc6a9;&#xbd88;&#xac00;"/>
+</node>
+<node CREATED="1539216516282" ID="ID_1805884133" MODIFIED="1539216516282" POSITION="right" TEXT="ng-options">
+<node COLOR="#3333ff" CREATED="1539216516283" ID="ID_413690793" MODIFIED="1539217123759" STYLE="bubble" TEXT="JS">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516284" ID="ID_836620873" MODIFIED="1539217123951" STYLE="bubble" TEXT="$scope.colors = [{name:&quot;red&quot;, id:1}, {name:&quot;blue&quot;, id:2},{name:&quot;green&quot;, id:3}]">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#3333ff" CREATED="1539216516286" ID="ID_1293721271" MODIFIED="1539217128260" POSITION="right" STYLE="bubble" TEXT="&lt;select ng-model=&quot;myColor&quot; ng-options=&quot;color.id as color.name for color in colors&quot;&gt;&lt;/select&gt;">
+<font BOLD="true" NAME="&#xad74;&#xb9bc;" SIZE="16"/>
+<node CREATED="1539216516288" ID="ID_1211344420" MODIFIED="1539217128254" TEXT="$scope.colors&#xc5d0; &#xc788;&#xb294; 3&#xac1c;&#xc758;&#xb370;&#xc774;&#xd130;&#xac00; &#xac01;&#xac01; color&#xc5d0; &#xb300;&#xc751;&#xb418;&#xace0;, &#xd654;&#xba74;&#xc5d0;&#xb294; color.name&#xac00; &#xd45c;&#xc2dc;&#xb418;&#xb294;&#xb370; &#xc120;&#xd0dd;&#xc2dc; ng-model&#xc778; myColor&#xc5d0;&#xb294; id&#xac00; &#xb2f4;&#xae34;&#xb2e4;."/>
+<node CREATED="1539216516295" ID="ID_1494214679" MODIFIED="1539216516295" TEXT="red"/>
+<node CREATED="1539216516296" ID="ID_1063949941" MODIFIED="1539216516296" TEXT="blue"/>
+<node CREATED="1539216516297" ID="ID_1399429281" MODIFIED="1539216516297" TEXT="green"/>
+</node>
+</node>
+</map>
